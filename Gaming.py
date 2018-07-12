de3bf13087ec85a94eba872a04a30940033425c1
Nir11s <Nir1012003@gmail.com>
2018-07-13 00:11:29 +0300

    Update Gaming.py

--- Gaming.py
+++ Gaming.py
@@ -57,7 +57,7 @@ async def on_message(message):
                     await client.send_message(message.channel, "**__Link:__**\n https://discord.gg/fZ3FcFP")
                     status = "ON"
                     time.sleep(86400)
-                    return
+                    return status
             else:
                 await client.send_message(message.channel, "<@%s> You do not have the premmision" % (userID))
                 return
